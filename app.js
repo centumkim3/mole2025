0b1c4fe9669ff9966f7e7d646718359b4c4ab27c
centumkim3 <135789797+centumkim3@users.noreply.github.com>
2025-10-17 10:47:45 +0900

    up

---
+++ app.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");
+
+let timeId = null;
+let hitPosition;
+let result = 0;
+let currentTime = 10;
+
+function randomSquare() {
+    squares.forEach((square) => {
+        square.classList.remove("mole");
+    });
+
+    let randomSquare = squares[Math.floor(Math.random()*9)];
+    randomSquare.classList.add("mole");
+    hitPosition = randomSquare.id;
+    
+}
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () =>{
+        if(square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition=null;
+        }
+    });
+});
+
+
+function moveMole() {
+    timeId = setInterval(randomSquare,500);
+}
+
+moveMole();
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+    if(currentTime == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timeId);
+        alert("Game Over! You final score is " + result);
+    }
+}
+
+
+
+let countDownTimerId = setInterval(countDown,1000);
